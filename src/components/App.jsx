a50a87c3193cbdccdda98a9700deeaf112f7a4f5
abach42 <arend.maubach@gmx.net>
2024-07-03 12:03:20 +0200

    componentDidMount und state()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import react from "react";
 
 export class App extends react.Component {
+    state = {
+        counterValue: 3
+    };
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState((state) => {
+                if(state.counterValue > 0) {
+                    return { counterValue: state.counterValue -1 };
+                } else {
+                    return { counterValue: 0 };
+                }
+            });
+        }, 1000);
+    }
+
     render() {
-        console.log(this.props);
-        const { title, name, nameObj} = this.props;
-        return <div title={title}>Hallo {nameObj.prename} {nameObj.lastname}</div>
+        return <strong>c = {this.state.counterValue}</strong>
     }
 }
