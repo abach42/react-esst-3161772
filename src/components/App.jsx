bd7508c8f5a54216af01a0c6411d75bed86827ed
abach42 <arend.maubach@gmx.net>
2024-07-03 12:11:33 +0200

    lifecycle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
+import { clear } from "@testing-library/user-event/dist/clear";
 import react from "react";
 
 export class App extends react.Component {
     state = {
-        counterValue: 3
+        counterValue: 5,
     };
 
     componentDidMount() {
-        setInterval(() => {
+        this.myInterval = setInterval(() => {
             this.setState((state) => {
                 if(state.counterValue > 0) {
                     return { counterValue: state.counterValue -1 };
@@ -19,5 +20,21 @@ export class App extends react.Component {
 
     render() {
         return <strong>c = {this.state.counterValue}</strong>
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevProps, prevState);
+    }
+
+    shouldComponentUpdate() {
+        if (this.state.counterValue === 0) {
+            return false;
+        }
+
+        return true;
     }
 }
