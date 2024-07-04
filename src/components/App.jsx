ee636aefa66dc51eb102b6f259c5cafb2adc1a28
abach42 <arend.maubach@gmx.net>
2024-07-04 12:12:31 +0200

    mystart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,3 @@
-import React, { useState } from "react";
-
 export function App() {
-  const [headline, setHeadline] = useState("");
-  const [inputValue, setInputValue] = useState("");
-
-  const buttonClicked = () => {
-    setHeadline(inputValue);
-  };
-
-  const inputChanged = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  return (
-    <div>
-      <h1>{headline}</h1>
-      <input type="text" onInput={inputChanged} />
-      <br />
-      <button type="button" onClick={buttonClicked}>
-        Make me big!
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 }
