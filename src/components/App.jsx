9d5ff830f6e18687a16b8c7439760079fad0513d
abach42 <arend.maubach@gmx.net>
2024-07-04 12:26:44 +0200

    event handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+
 export function App() {
-  return <div>App</div>;
+  const [headline, setHeadline] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  //use input value state to set into headline var
+  const buttonClicked = () => {
+    setHeadline(inputValue);
+  }
+
+  //on input: write input value to state
+  const inputChanged = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <h1>{headline}</h1>
+      <input type="text" onInput={inputChanged} />
+      <br />
+      <button type="button" onClick={buttonClicked}>
+        Make me big!
+      </button>
+    </div>
+  );
 }
