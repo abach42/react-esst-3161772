6b06ddc6d2cfcf6836e9b92c56e76c72dc42d56d
abach42 <arend.maubach@gmx.net>
2024-07-04 16:50:17 +0200

    wetterstation im context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,52 @@
-import React from "react";
-import { BusinessProfile } from "./BusinessProfile";
-import { OpenToWorkContext } from "../OpenToWorkContext";
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import { Weather } from "./Weather";
+import { WeatherContext } from "../WeatherContext";
 
 export function App() {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWeather((prevWeatherObject) => {
+        return {
+          ...prevWeatherObject,
+          Berlin: "rainy 🌧️",
+        };
+      });
+    }, 500);
+
+    setTimeout(() => {
+      setWeather((prevWeatherObject) => {
+        return {
+          ...prevWeatherObject,
+          Bonn: "sunny ☀️",
+        };
+      });
+    }, 1000);
+
+    setTimeout(() => {
+      setWeather((prevWeatherObject) => {
+        return {
+          ...prevWeatherObject,
+          'Frankfurt am Main': "hot and cold ⛅",
+        };
+      });
+    }, 1000);
+
+  }, []);
+
   return (
     <div>
-      <h1>Contexts &amp; Theming</h1>
+      <h1>⛅ Wetta</h1>
 
-      <BusinessProfile
-        name="Johana Doe"
-        job="Frontend Designer:inneninneneden"
-      />
-
-      <OpenToWorkContext.Provider value={true}>
-        <div>
-          <h2>Opfen to work: </h2>
-
-          <BusinessProfile
-            name="Johana Doe"
-            job="Frontend Designer:inneninneneden"
-          />
-
-          <BusinessProfile
-            name="Johana Doe"
-            job="Frontend Designer:inneninneneden"
-          />
-        </div>
-      </OpenToWorkContext.Provider>
+      <WeatherContext.Provider value={weather}>
+        <Weather city="Berlin" />
+        <Weather city="Frankfurt am Main" />
+        <Weather city="Bonn" />
+      </WeatherContext.Provider>
     </div>
   );
 }
