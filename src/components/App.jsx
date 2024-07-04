f7272ed0fb7e2f3d103e0be661f79b068ef171ea
abach42 <arend.maubach@gmx.net>
2024-07-04 16:29:24 +0200

    simple context example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { BusinessProfile } from "./BusinessProfile";
+import { OpenToWorkContext } from "../OpenToWorkContext";
 
 export function App() {
   return (
     <div>
       <h1>Contexts &amp; Theming</h1>
 
+      <BusinessProfile
+        name="Johana Doe"
+        job="Frontend Designer:inneninneneden"
+      />
+
+      <OpenToWorkContext.Provider value={true}>
+        <div>
+          <h2>Opfen to work: </h2>
+
+          <BusinessProfile
+            name="Johana Doe"
+            job="Frontend Designer:inneninneneden"
+          />
+
+          <BusinessProfile
+            name="Johana Doe"
+            job="Frontend Designer:inneninneneden"
+          />
+        </div>
+      </OpenToWorkContext.Provider>
     </div>
   );
 }
